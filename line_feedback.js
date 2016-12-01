47287526a690fc06d795df8a4a33eea30a78acd7
intheblackworld <intheblackworld@intheblackworlddeMacBook-Pro.local>
2016-12-01 23:54:12 +0800

    update fifth media line feedback

--- line_feedback.js
+++ line_feedback.js
@@ -4,16 +4,16 @@
 // 2          1
 // 影片序號 + 編號
 var data = [
-    { id: 501, url: 'http://yesbrain.org/2016/11/yong-yi-ge-gu-shi-lai-shuo-ming-he-wei-yong-qian-h/'},
-    { id: 502, url: 'https://www.youtube.com/watch?v=RBLrPUGSaV4&t=2s'},
-    { id: 503, url: 'http://www.gegugu.com/2016/04/13/11216.html'},
+    { id: "051", url: 'http://yesbrain.org/2016/11/yong-yi-ge-gu-shi-lai-shuo-ming-he-wei-yong-qian-h/'},
+    { id: '052', url: 'https://www.youtube.com/watch?v=RBLrPUGSaV4&t=2s'},
+    { id: "053", url: 'http://www.gegugu.com/2016/04/13/11216.html'},
 ]
 
 $(document).ready(function() {
     $(".produce_line").on("click", function() {
         new Clipboard('.btn');
         var value = $(".input_line").val();
-        var result = _.find(data, {id: +value}).url
+        var result = _.find(data, {id: value}).url
         $('.result').html(result);
     })
 });
